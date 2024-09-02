fe4d1ed3d34e16a1074369e5fda04eb972cd2c3a
Petr Popov <petrpopovp@yandex.ru>
2024-09-02 13:33:38 +0300

    second commit form idea

--- src/main/java/com/stepup/proj02/MainApp.java
+++ src/main/java/com/stepup/proj02/MainApp.java
@@ -6,5 +6,6 @@ package com.stepup.proj02;
 public class MainApp {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("11111");
     }
 }
